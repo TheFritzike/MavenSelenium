9d23e4e63881844fc745cbc0df0621009825cc37
TheFritzike <ferenczlevente88@gmail.com>
2023-01-30 22:21:48 +0200

    Ket teszt van

--- src/test/java/TestRun.java
+++ src/test/java/TestRun.java
@@ -1,34 +1,39 @@
 import org.junit.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.time.Duration;
 
 public class TestRun {
-    @Test
-    public void sauceDemoPage(){
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromeDriver/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--headless");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        options.merge(capabilities);
-        ChromeDriver driver = new ChromeDriver(options);
+    public static WebDriver driver = null;
+    @BeforeClass
+    public static void startTest(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         System.out.println("Test Start.");
+        driver.get("https://www.saucedemo.com/");
+    }
+    @AfterClass
+    public static void tearDown(){
+        System.out.println("Test End.");
+        driver.close();
+    }
 
-        //variables
+    @Test
+    @DisplayName("Login Test.")
+    @Order(1)
+    public void loginTest(){
         By user = By.id("user-name");
         By pass = By.id("password");
 
-        //open page verify title
-        driver.get("https://www.saucedemo.com/");
         String pageTitle = driver.getTitle();
         Assert.assertEquals(pageTitle,"Swag Labs");
 
-        //insert user, password and press Enter
         WebElement userInput = driver.findElement(user);
         WebElement passInput = driver.findElement(pass);
         userInput.clear();
@@ -36,9 +41,19 @@ public class TestRun {
         passInput.clear();
         passInput.sendKeys("secret_sauce");
         passInput.sendKeys(Keys.ENTER);
+    }
 
-        //end test
-        System.out.println("Test End.");
-        driver.close();
+    @Test
+    @DisplayName("Add Product Test")
+    @Order(2)
+    public void addProductToBasket(){
+        By tShirt = By.id("add-to-cart-sauce-labs-bolt-t-shirt");
+        By cartQuantity = By.xpath("//span[@class='shopping_cart_badge']");
+
+        WebElement addShirtToBasket = driver.findElement(tShirt);
+        addShirtToBasket.click();
+
+        String quantity = driver.findElement(cartQuantity).getText();
+        Assert.assertEquals(1,Integer.parseInt(quantity));
     }
 }
